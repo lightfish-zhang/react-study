e7f2c7c88b5da69e9c553269031a92017853b531
zhangguangyu <coolyeah@foxmail.com>
2017-04-28 19:01:15 +0800

    07

---
+++ 07-element-instance/server.js
@@ -0,0 +1,37 @@
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+const config = require('./webpack.config');
+const Express = require('express');
+const path = require('path');
+
+const app = new Express();
+const port = 3000;
+
+const compiler = webpack(config);
+
+// express.use(path, function) 在path的请求上都会执行function
+// express.use(function) 在所有请求上都会执行function
+app.use(webpackDevMiddleware(
+    compiler,
+    {
+        noInfo: true,
+        publicPath: config.output.publicPath
+    })
+);
+
+// now compiler and output ./static/bundle.js in web server
+app.use(webpackHotMiddleware(compiler));
+
+
+app.get('/', (request, result) => {
+    result.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.listen(port, (error) => {
+    if (error) {
+        console.error(error);
+    } else {
+        console.info('==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.', port, port);
+    }
+});
