c316dee7b6d355af30fd0fb9f6bf050736f85ec1
zhangguangyu <coolyeah@foxmail.com>
2017-05-05 12:01:54 +0800

    react event

--- 06-state-props-context/src/Counter.js
+++ 06-state-props-context/src/Counter.js
@@ -28,3 +28,15 @@ export default class Counter extends Component {
         );
     }
 }
+
+/*
+# React 事件系统 `合成事件: 事件委派和自动绑定`
+- virtual DOM 在内存中是以对象形式存在的
+- React基于Virtual DOM实现了一个SyntheticEvent(合成事件)层
+- 所有事件自动绑定到最外层，如需访问原声事件对象，使用nativeEvent属性
+- 在JSX中，使用驼峰书写事件的属性名，如onClick，在HTML中，使用全小写，如onclick
+## 事件委派
+- 事件代理机制，不直接绑定事件处理函数到真实节点，而是绑定到结构的最外层
+- 使用统一的事件监听器，维持一个映射来保存所有组件内部的事件监听和处理函数
+
+*/
